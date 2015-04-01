fe3f7841112ed6935c6be636ab2ade8f47bb4390
Dann S Washko <dwashko@gmti.gannett.com>
2015-04-01 13:47:32 -0700

    GDAES-392 - incrementing version of cookbook to 2.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "jdunn@opscode.com"
 license          "MIT"
 description      "Installs/Configures Couchbase"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
-version          "1.0.1"
+version          "2.0.0"
 
 %w(debian ubuntu centos redhat oracle amazon scientific windows).each do |os|
   supports os
