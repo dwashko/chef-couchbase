29546eca0b2359b5048a09e10cd8680f8232ea1e
Dann S Washko <dwashko@gmti.gannett.com>
2015-11-20 16:52:25 -0500

    GDPHAR-1701 fixing issues with index path not being set properly

--- libraries/node_provider.rb
+++ libraries/node_provider.rb
@@ -12,14 +12,12 @@ class Chef
       def load_current_resource
         @current_resource = Chef::Resource::CouchbaseNode.new @new_resource.name
         @current_resource.id @new_resource.id
-        @current_resource.database_path @new_resource.database_path # node_database_path
-        @current_resource.index_path @new_resource.index_path # node_index_path
+        @current_resource.database_path node_database_path
+        @current_resource.index_path node_index_path
       end
 
       def action_modify
-        Chef::Log.warn("setting indexpath to ")
-        Chef::Log.warn(@new_resource.index_path)
-        return unless @current_resource.database_path != @new_resource.database_path
+        return unless @current_resource.database_path != @new_resource.database_path || @current_resource.index_path != @new_resource.index_path
         post "/nodes/#{@new_resource.id}/controller/settings", 'path' => @new_resource.database_path,
         'index_path' => @new_resource.index_path
         @new_resource.updated_by_last_action true
@@ -33,7 +31,7 @@ class Chef
       end
 
       def node_index_path
-        node_data['storage']['hdd'][0]['path']
+        node_data['storage']['hdd'][0]['index_path']
       end
 
       def node_data
