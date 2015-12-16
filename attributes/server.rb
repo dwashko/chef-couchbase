77e28e3683b1758cf3c26b8652784d3828ff5a64
Dann S Washko <dwashko@gmti.gannett.com>
2015-12-16 10:55:48 -0500

    GDPHAR-1871 - cleaning up attributes for server

--- attributes/server.rb
+++ attributes/server.rb
@@ -13,42 +13,6 @@ default['couchbase']['server']['services_api'] = 'kv,n1ql,index'
 
 default['couchbase']['server']['port'] = 8091
 
-case node['platform']
-when 'debian'
-  package_machine = node['kernel']['machine'] == 'x86_64' ? 'amd64' : 'x86'
-  if node['couchbase']['server']['version'] < '3.0.0'
-    Chef::Log.error("Couchbase Server does not have a Debian release for #{node['couchbase']['server']['version']}")
-  else
-    default['couchbase']['server']['package_file'] = "couchbase-server-#{node['couchbase']['server']['edition']}_#{node['couchbase']['server']['version']}-debian7_#{package_machine}.deb"
-  end
-when 'centos', 'redhat', 'amazon', 'scientific'
-  package_machine = node['kernel']['machine'] == 'x86_64' ? 'x86_64' : 'x86'
-  if node['couchbase']['server']['version'] < '3.0.0'
-    default['couchbase']['server']['package_file'] = "couchbase-server-#{node['couchbase']['server']['edition']}_#{node['couchbase']['server']['version']}_#{package_machine}.rpm"
-  else
-    default['couchbase']['server']['package_file'] = "couchbase-server-#{node['couchbase']['server']['edition']}-#{node['couchbase']['server']['version']}-centos6.#{package_machine}.rpm"
-  end
-when 'ubuntu'
-  package_machine = node['kernel']['machine'] == 'x86_64' ? 'amd64' : 'x86'
-  if node['couchbase']['server']['version'] < '3.0.0'
-    default['couchbase']['server']['package_file'] = "couchbase-server-#{node['couchbase']['server']['edition']}_#{node['couchbase']['server']['version']}_#{package_machine}.deb"
-  else
-    default['couchbase']['server']['package_file'] = "couchbase-server-#{node['couchbase']['server']['edition']}_#{node['couchbase']['server']['version']}-ubuntu12.04_#{package_machine}.deb"
-  end
-when 'windows'
-  if node['kernel']['machine'] != 'x86_64'
-    Chef::Log.error('Couchbase Server on Windows must be installed on a 64-bit machine')
-  else
-    default['couchbase']['server']['version'] = '3.0.0-beta'
-    default['couchbase']['server']['package_file'] = "couchbase-server_#{node['couchbase']['server']['version']}-beta-windows_amd64.exe"
-  end
-else
-  Chef::Log.error("Couchbase Server is not supported on #{node['platform_family']}")
-end
-
-default['couchbase']['server']['package_base_url'] = "http://packages.couchbase.com/releases/#{node['couchbase']['server']['version']}"
-default['couchbase']['server']['package_full_url'] = "#{node['couchbase']['server']['package_base_url']}/#{node['couchbase']['server']['package_file']}"
-
 case node['platform_family']
 when 'windows'
   default['couchbase']['server']['service_name'] = 'CouchbaseServer'
@@ -62,8 +26,6 @@ default['couchbase']['server']['database_path'] = File.join(node['couchbase']['s
 default['couchbase']['server']['index_path'] = File.join(node['couchbase']['server']['install_dir'], 'var', 'lib', 'couchbase', 'data')
 default['couchbase']['server']['log_dir'] = File.join(node['couchbase']['server']['install_dir'], 'var', 'lib', 'couchbase', 'logs')
 
-default['couchbase']['server']['source']['bucket'] = 'default'
-default['couchbase']['server']['remote']['bucket'] = 'default'
 default['couchbase']['server']['setup_cluster'] = false
 default['couchbase']['server']['run_cluster_init'] = true
 default['couchbase']['server']['cluster_name'] = 'west_cluster'
