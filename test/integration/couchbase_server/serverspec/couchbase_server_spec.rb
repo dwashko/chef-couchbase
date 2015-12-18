a951d4dcb9a2021447538fc4b1c41e0e2d7b0f78
Dann S Washko <dwashko@gmti.gannett.com>
2015-12-18 17:15:29 -0500

    GDPHAR-1871 - adding bucket tests

--- test/integration/couchbase_server/serverspec/couchbase_server_spec.rb
+++ test/integration/couchbase_server/serverspec/couchbase_server_spec.rb
@@ -21,12 +21,12 @@ describe port(8091) do
 end
 
 bucket_output = [
-  %r{/test/},
-  %r{/ bucketType: membase/},
-  %r{/ authType: sasl/},
-  %r{/ numReplicas: 0/},
-  %r{/ ramQuota: 130023424/},
-  %r{/ ramUsed: 38267248/}
+  /test/,
+  / bucketType: membase/,
+  / authType: sasl/,
+  / numReplicas: 0/,
+  / ramQuota: 130023424/,
+  / ramUsed: 38267248/
 ]
 describe command('/opt/couchbase/bin/couchbase-cli bucket-list -c 127.0.0.1:8091 -u Administrator -p password') do
   bucket_output.each do |b|
